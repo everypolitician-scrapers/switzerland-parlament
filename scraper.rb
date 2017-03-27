80709baa764e5cef7c86f5d1141ea5752bd53fce
Tony Bowden <tony@tmtm.com>
2017-03-27 15:35:57 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -7,7 +8,7 @@ require 'open-uri'
 require 'json'
 
 def json_from(url)
-  JSON.parse(open(url, "Accept" => "text/json").read, symbolize_names: true)
+  JSON.parse(open(url, 'Accept' => 'text/json').read, symbolize_names: true)
 end
 
 def gender_from(str)
@@ -18,8 +19,8 @@ def gender_from(str)
 end
 
 def scrape_term(t)
-  return if t[:id] > 50  # or if the term start date is in the future
-  base = "http://ws-old.parlament.ch/councillors/historic?legislativePeriodFromFilter=#{t[:id]}&format=json&pageNumber=%d" 
+  return if t[:id] > 50 # or if the term start date is in the future
+  base = "http://ws-old.parlament.ch/councillors/historic?legislativePeriodFromFilter=#{t[:id]}&format=json&pageNumber=%d"
 
   page = 0
   while page += 1
@@ -33,47 +34,45 @@ def scrape_term(t)
 end
 
 def scrape_person(mp, term)
-  data = { 
-    id: mp[:id],
+  data = {
+    id:                         mp[:id],
     identifier__parlamentdotch: mp[:id],
-    name: mp[:firstName] + " " + mp[:lastName],
-    sort_name: mp[:lastName] + ", " + mp[:firstName],
-    given_name: mp[:firstName],
-    family_name: mp[:lastName],
-    birth_date: mp[:birthDate].slice!(0,10),
-    gender: gender_from(mp[:gender]),
-    area: mp[:canton][:abbreviation],
-    area_id: mp[:canton][:id],
-    council: mp[:council][:abbreviation],
-    council_id: mp[:council][:id],
-    party: mp[:party][:abbreviation],
-    party_id: mp[:party][:id],
-    faction: mp[:faction][:abbreviation],
-    faction_id: mp[:faction][:id],
-    term: term[:id],
-    source: "https://www.parlament.ch/en/biografie?CouncillorId=#{mp[:id]}",
+    name:                       mp[:firstName] + ' ' + mp[:lastName],
+    sort_name:                  mp[:lastName] + ', ' + mp[:firstName],
+    given_name:                 mp[:firstName],
+    family_name:                mp[:lastName],
+    birth_date:                 mp[:birthDate].slice!(0, 10),
+    gender:                     gender_from(mp[:gender]),
+    area:                       mp[:canton][:abbreviation],
+    area_id:                    mp[:canton][:id],
+    council:                    mp[:council][:abbreviation],
+    council_id:                 mp[:council][:id],
+    party:                      mp[:party][:abbreviation],
+    party_id:                   mp[:party][:id],
+    faction:                    mp[:faction][:abbreviation],
+    faction_id:                 mp[:faction][:id],
+    term:                       term[:id],
+    source:                     "https://www.parlament.ch/en/biografie?CouncillorId=#{mp[:id]}",
   }
 
   if mp[:membership][:entryDate]
-    start_date = mp[:membership][:entryDate].slice(0,10) 
+    start_date = mp[:membership][:entryDate].slice(0, 10)
     data[:start_date] = start_date if start_date > term[:start_date]
   end
 
   if mp[:membership][:leavingDate]
-    end_date = mp[:membership][:leavingDate].slice(0,10) 
+    end_date = mp[:membership][:leavingDate].slice(0, 10)
     data[:end_date] = end_date if end_date < term[:end_date]
   end
 
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
-
-
 
 terms = json_from('http://ws-old.parlament.ch/legislativeperiods?format=json')
 
 terms.each do |t|
-  t[:start_date] = (t.delete :from).slice!(0,10)
-  t[:end_date] = (t.delete :to).slice!(0,10)
+  t[:start_date] = (t.delete :from).slice!(0, 10)
+  t[:end_date] = (t.delete :to).slice!(0, 10)
   %i(hasMorePages updated code).each { |i| t.delete i }
   t.delete :hasMorePages
   t.delete :updated
@@ -81,4 +80,3 @@ terms.each do |t|
   ScraperWiki.save_sqlite([:id], t, 'terms')
   scrape_term(t)
 end
-
